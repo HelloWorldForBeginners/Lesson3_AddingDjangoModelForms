e552857288a51a152c293e3514adcbcb985a6f18
matt <odle.matthew@gmail.com>
2016-02-20 01:14:17 -0600

    branching for heroku configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd3693e62mclhjn',
+        'USER': 'jqypoiekjmbmyq',
+        'PASSWORD': 'Ca0cH3gHm4e0Lav3t3oWcQnpoI',
+        'HOST': 'ec2-107-20-153-141.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
@@ -118,4 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/mysite/staticfiles/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
