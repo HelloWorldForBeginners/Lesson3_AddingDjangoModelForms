952f24eb15d2d86d72a5261414536608e1158087
matt <odle.matthew@gmail.com>
2016-02-28 16:02:37 -0600

    adding the blog model

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    body = models.TextField()
+    post_date = models.DateTimeField(default=timezone.now)
+
